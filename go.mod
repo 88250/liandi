820c1fe0e037824b5184ff3153c1737ddc08cf03
Liang Ding <d@b3log.org>
2020-01-25 17:57:52 +0800

    :art: 加载配置

--- go.mod
+++ go.mod
@@ -3,10 +3,21 @@ module github.com/88250/liandi
 go 1.12
 
 require (
-	github.com/88250/gulu v0.0.0-20191221090949-7ec528e17061
+	github.com/88250/gulu v1.0.0
 	github.com/gin-gonic/gin v1.5.0
+	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/gorilla/websocket v1.4.1 // indirect
+	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/kr/pretty v0.2.0 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/mitchellh/go-ps v0.0.0-20190716172923-621e5597135b
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa
+	golang.org/x/sys v0.0.0-20200124204421-9fbb57f87de9 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
 	gopkg.in/olahol/melody.v1 v1.0.0-20170518105555-d52139073376
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- util/conf.go
+++ util/conf.go
@@ -13,6 +13,8 @@
 package util
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 
@@ -30,16 +32,49 @@ var (
 	logger     = gulu.Log.NewLogger(os.Stdout)
 	HomeDir, _ = gulu.OS.Home()
 	LianDiDir  = filepath.Join(HomeDir, ".liandi")
+	ConfPath   = filepath.Join(LianDiDir, "conf.json")
 )
+
+var Conf *AppConf
 
 func InitConf() {
 	os.Mkdir(LianDiDir, 0755)
+
+	Conf = &AppConf{}
+
+	if !gulu.File.IsExist(ConfPath) {
+		saveConf()
+		logger.Infof("初始化配置文件 [%s] 完毕", ConfPath)
+	} else {
+		data, err := ioutil.ReadFile(ConfPath)
+		if nil != err {
+			logger.Fatalf("加载配置文件 [%s] 失败：%s", ConfPath, err)
+		}
+		err = json.Unmarshal(data, Conf)
+		if err != nil {
+			logger.Fatalf("解析配置文件 [%s] 失败：%s", ConfPath, err)
+		}
+		logger.Debugf("加载配置文件 [%s] 完毕", ConfPath)
+	}
+
+}
+
+func saveConf() {
+	data, _ := json.Marshal(Conf)
+	if err := ioutil.WriteFile(ConfPath, data, 0644); nil != err {
+		logger.Fatalf("写入配置文件 [%s] 失败：", ConfPath, err)
+	}
 }
 
 // AppConf 维护应用元数据，保存在 ~/.liandi/conf.json ，记录已经打开的文件夹、各种配置项等。
 type AppConf struct {
+	Dirs []*Dir `json:"dirs"`
 }
 
-// DirConf 维护文件夹元数据，保存在打开文件夹下 .liandi/conf.json ，记录文件展开状态、编辑状态等。
-type DirConf struct {
+// Dir 维护了打开的 WebDAV 目录。
+type Dir struct {
+	URL      string `json:"url"`
+	auth     bool   `json:"auth"`
+	username string `json:"username"`
+	password string `json:"password"`
 }
