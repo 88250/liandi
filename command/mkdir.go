8fe6921d3c16c0e626186fd28bec00ee3e2dad57
Liang Ding <d@b3log.org>
2020-01-30 14:35:08 +0800

    :art: webdav

--- command/mkdir.go
+++ command/mkdir.go
@@ -12,7 +12,11 @@
 
 package command
 
-import "github.com/88250/liandi/util"
+import (
+	"path"
+
+	"github.com/88250/liandi/util"
+)
 
 type mkdir struct {
 	*BaseCmd
@@ -22,11 +26,15 @@ func (cmd *mkdir) Exec() {
 	ret := util.NewCmdResult(cmd.Name())
 	url := cmd.param["url"].(string)
 	url = util.NormalizeURL(url)
-	path := cmd.param["path"].(string)
-	err := util.Mkdir(url, path)
+	p := cmd.param["path"].(string)
+	err := util.Mkdir(url, p)
 	if nil != err {
 		ret.Code = -1
 		ret.Msg = err.Error()
+	}
+	ret.Data = map[string]interface{}{
+		"url":  url,
+		"path": path.Dir(p),
 	}
 	util.Push(ret.Bytes())
 }
