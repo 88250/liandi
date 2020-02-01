26e37cc010e267f935eae82cb45ff535901356b4
Van <lly219@gmail.com>
2020-02-01 23:52:11 +0800

    :sparkles: stop link

--- app/electron/main.js
+++ app/electron/main.js
@@ -1,16 +1,24 @@
-const { app, BrowserWindow } = require('electron')
+const {app, BrowserWindow, shell} = require('electron')
+
 function createWindow () {
   // 创建浏览器窗口
-  let win = new BrowserWindow({
+  let mainWindow = new BrowserWindow({
     width: 1024,
     height: 768,
     webPreferences: {
-      nodeIntegration: true
-    }
+      nodeIntegration: true,
+    },
   })
 
   // 加载index.html文件
-  win.loadFile('../public/index.html')
+  mainWindow.loadFile('../public/index.html')
+
+  // 组织当前页面链接跳转
+  mainWindow.webContents.on('will-navigate', (e, url) => {
+    e.preventDefault()
+    console.log(e, url)
+    shell.openExternal(url)
+  })
 }
 
 app.on('ready', createWindow)
