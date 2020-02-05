471272989a7eaa0407ded823795b150f47bf9ac9
Van <lly219@gmail.com>
2020-02-05 13:42:52 +0800

    :sparkles: config search

--- app/src/config/search.ts
+++ app/src/config/search.ts
@@ -1,20 +1,40 @@
 export const initConfigSearch = (liandi: ILiandi, element: HTMLElement) => {
     const configIndex = [
         ["markdown"],
-        ['white', 'dark', 'theme'],
-        ['zh_CN', 'en_US', 'language']
+        ['white', 'dark', 'theme', '主题'],
+        ['zh_CN', 'en_US', 'language', '语言']
     ]
     const inputElement = element.querySelector('.input') as HTMLInputElement
     const updateTab = () => {
-        const tabElements = element.querySelectorAll('.tab--vertical li')
-        let index = []
+        let indexList: number[] = []
         const inputValue = inputElement.value
         configIndex.map((item, index) => {
             item.map(subItem => {
-                // if ()
+                if (inputValue.toLowerCase().indexOf(subItem) > -1 || subItem.toLowerCase().indexOf(inputValue) > -1) {
+                    indexList.push(index)
+                }
             })
         })
 
+        let currentTabElement: HTMLElement
+        element.querySelectorAll('.tab--vertical li').forEach((item: HTMLElement, index) => {
+            if (indexList.includes(index)) {
+                if (!currentTabElement) {
+                    currentTabElement = item
+                }
+                item.style.display = 'block'
+            } else {
+                item.style.display = 'none'
+            }
+        })
+
+        const tabPanelElement = element.querySelector('tab-panel') as HTMLElement
+        if (currentTabElement) {
+            tabPanelElement.style.display = 'block'
+            currentTabElement.click()
+        } else {
+            tabPanelElement.style.display = 'none'
+        }
     }
 
     inputElement.addEventListener('compositionend', () => {
