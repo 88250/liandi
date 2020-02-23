4385e901544f7e284980230d00c938a56179a73a
Van <lly219@gmail.com>
2020-02-23 13:24:44 +0800

    :lipstick: menu

--- app/electron/main.js
+++ app/electron/main.js
@@ -1,13 +1,14 @@
-const {app, BrowserWindow, shell, Menu, globalShortcut, screen} = require('electron')
+const {app, BrowserWindow, shell, Menu, globalShortcut, screen} = require(
+  'electron')
 const {spawn} = require('child_process')
 const path = require('path')
 const os = require('os')
 const fs = require('fs')
 process.noAsar = true
 const homedir = os.homedir()
-const liandi = path.join(homedir, ".liandi")
+const liandi = path.join(homedir, '.liandi')
 const appDir = path.dirname(app.getAppPath())
-const current = path.join(liandi, "current")
+const current = path.join(liandi, 'current')
 const getKernelName = () => {
   let ret = 'kernel.exe'
   if (process.platform === 'darwin') {
@@ -49,23 +50,44 @@ const createWindow = () => {
   if (isDevEnv) {
     mainWindow.webContents.openDevTools({mode: 'bottom'})
   } else {
+    const productName = "链滴笔记"
     const template = [
       {
-        label: '链滴笔记',
+        label: productName,
         submenu: [
-          {role: 'about'},
+          {
+            label: `About ${productName}`,
+            role: 'about'
+          },
           {type: 'separator'},
-          {role: 'cut'},
-          {role: 'copy'},
-          {role: 'paste'},
+          {role: 'services'},
+          {type: 'separator'},
+          {
+            label: `Hide ${productName}`,
+            role: 'hide'
+          },
+          {role: 'hideOthers'},
+          {role: 'unhide'},
+          {type: 'separator'},
+          {
+            label: `Quit ${productName}`,
+            role: 'quit'
+          },
+        ],
+      },
+      {role: 'editMenu'},
+      {
+        role: 'windowMenu',
+        submenu: [
+          {role: 'minimize'},
+          {role: 'zoom'},
+          {role: 'togglefullscreen'},
+          {role: 'close'},
           {type: 'separator'},
           {role: 'toggledevtools'},
           {type: 'separator'},
-          {role: 'togglefullscreen'},
-          {role: 'minimize'},
-          {role: 'close'},
-          {role: 'quit'},
-        ],
+          {role: 'front'},
+        ]
       },
     ]
     const menu = Menu.buildFromTemplate(template)
@@ -97,7 +119,7 @@ const createWindow = () => {
 
   global.liandiEditor = {
     editorText: '',
-    saved: true
+    saved: true,
   }
 }
 
@@ -110,7 +132,7 @@ const applyUpdate = () => {
     fs.mkdirSync(liandi)
     copyDir(appDir, current)
   } else {
-    const update = path.join(liandi, "update")
+    const update = path.join(liandi, 'update')
     if (fs.existsSync(update)) {
       removeDir(current)
       fs.renameSync(update, current)
@@ -153,26 +175,26 @@ app.on('will-quit', () => {
 app.on('web-contents-created', (webContentsCreatedEvent, contents) => {
   if (contents.getType() === 'webview') {
     contents.on('new-window', (newWindowEvent, url) => {
-      newWindowEvent.preventDefault();
-      shell.openExternal(url);
-    });
+      newWindowEvent.preventDefault()
+      shell.openExternal(url)
+    })
   }
-});
+})
 
 const removeDir = function (dirPath) {
   if (!fs.existsSync(dirPath)) {
     return
   }
 
-  fs.readdirSync(dirPath).forEach((file, index) => {
-    const curPath = path.join(dirPath, file);
+  fs.readdirSync(dirPath).forEach((file) => {
+    const curPath = path.join(dirPath, file)
     if (fs.lstatSync(curPath).isDirectory()) {
-      removeDir(curPath);
+      removeDir(curPath)
     } else {
-      fs.unlinkSync(curPath);
+      fs.unlinkSync(curPath)
     }
-  });
-  fs.rmdirSync(dirPath);
+  })
+  fs.rmdirSync(dirPath)
 }
 
 const copyDir = function (src, dest) {
@@ -180,16 +202,16 @@ const copyDir = function (src, dest) {
     return
   }
 
-  if (".asar" === path.extname(src)) {
+  if ('.asar' === path.extname(src)) {
     return
   }
 
   if (fs.lstatSync(src).isDirectory()) {
-    fs.mkdirSync(dest);
+    fs.mkdirSync(dest)
     fs.readdirSync(src).forEach(childItemName => {
-      copyDir(path.join(src, childItemName), path.join(dest, childItemName));
-    });
+      copyDir(path.join(src, childItemName), path.join(dest, childItemName))
+    })
   } else {
-    fs.copyFileSync(src, dest);
+    fs.copyFileSync(src, dest)
   }
-};
+}
