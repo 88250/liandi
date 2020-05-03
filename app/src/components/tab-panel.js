048175ef756610299b98be0fd0cd734e873649ed
Liyuan Li <v@b3log.org>
2020-05-03 23:48:59 +0800

    :bug: tab panel

--- app/src/components/tab-panel.js
+++ app/src/components/tab-panel.js
@@ -20,21 +20,31 @@ customElements.define('tab-panel',
 
       tabs.forEach(item => {
         item.addEventListener('click', () => {
-          tabs.forEach(item => {
-            item.classList.remove('tab--current')
+          tabs.forEach(tabItem => {
+            tabItem.classList.remove('tab--current')
           })
+          item.classList.add('tab--current')
 
           panels.forEach(description => {
-            description.removeAttribute('slot')
-
             if (description.getAttribute('data-name') ===
               item.getAttribute('data-name')) {
+
               description.setAttribute('slot', 'panel')
-              item.classList.add('tab--current')
+
               const inputElement = description.querySelector('input')
               if (inputElement) {
                 inputElement.focus()
+              } else {
+                // 如果 select 选中的话，切换 description 不会替换
+                item.focus()
               }
+            }
+          })
+          // 如果先移除 slot 的话，tab--current 会失效
+          panels.forEach(description => {
+            if (description.getAttribute('data-name') !==
+              item.getAttribute('data-name')) {
+              description.removeAttribute('slot')
             }
           })
         })
