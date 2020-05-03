722b2e1b31483cfaae224c881497f819e02bce3b
Liyuan Li <v@b3log.org>
2020-05-04 01:20:03 +0800

    :art: double shift

--- app/src/hotkey/index.ts
+++ app/src/hotkey/index.ts
@@ -4,7 +4,7 @@ import {initSearch} from '../search';
 
 export const initGlobalKeyPress = (liandi: ILiandi | any) => {
     let lastKeypressTime = 0;
-
+    let timeoutId = 0;
     window.addEventListener('keydown', (event) => {
         // 快捷搜素
         if (event.key === 'Shift') {
@@ -12,16 +12,19 @@ export const initGlobalKeyPress = (liandi: ILiandi | any) => {
             if (thisKeypressTime - lastKeypressTime <= Constants.DOUBLE_DELTA
                 && thisKeypressTime - lastKeypressTime >= 50) { // 防止 win32 长按
                 lastKeypressTime = 0;
-                if (liandi.range) {
-                    liandi.range = getSelection().getRangeAt(0);
-                    ipcRenderer.sendToHost(Constants.LIANDI_SEARCH_OPEN);
-                } else {
-                    initSearch(liandi);
-                }
+                timeoutId = window.setTimeout(() => {
+                    if (liandi.range) {
+                        liandi.range = getSelection().getRangeAt(0);
+                        ipcRenderer.sendToHost(Constants.LIANDI_SEARCH_OPEN);
+                    } else {
+                        initSearch(liandi);
+                    }
+                }, 200)
             }
             lastKeypressTime = thisKeypressTime;
         } else {
             lastKeypressTime = 0;
+            clearTimeout(timeoutId);
         }
     });
 };
