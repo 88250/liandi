f8fc0d4233bb00183bca9a41ed718ccd0cb37679
Liyuan Li <v@b3log.org>
2020-08-03 20:44:02 +0800

    :art: #87 add vditore

--- app/src/editors/webview.ts
+++ app/src/editors/webview.ts
@@ -148,7 +148,7 @@ export class EditorWebview {
                             if ('vditor'.indexOf(key.toLocaleLowerCase()) > -1) {
                                 return [
                                     {
-                                        value: '((Vditor',
+                                        value: '((1596452954774))',
                                         html: '<span style="color: #999;">#Vditor</span> ♏ 一款浏览器端的 Markdown 编辑器，支持所见即所得（富文本）、即时渲染（类似 Typora）和分屏预览模式。',
                                     }]
                             }
