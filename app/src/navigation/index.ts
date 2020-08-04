fdfba8cd784e3b21ba9328eb69f01324364a2d1f
Liyuan Li <v@b3log.org>
2020-08-04 21:54:08 +0800

    :recycle: #87

--- app/src/navigation/index.ts
+++ app/src/navigation/index.ts
@@ -28,7 +28,7 @@ export class Navigation {
                 .querySelector(`.tree-list__arrow[path="${data.path}"]`);
             arrowElement.setAttribute('has-file', 'true');
             arrowElement.setAttribute('files', JSON.stringify(data.files));
-            arrowElement.innerHTML = '<path d="M25 16c0 0.531-0.219 1.031-0.594 1.406l-14 14c-0.375 0.375-0.875 0.594-1.406 0.594-1.094 0-2-0.906-2-2v-28c0-1.094 0.906-2 2-2 0.531 0 1.031 0.219 1.406 0.594l14 14c0.375 0.375 0.594 0.875 0.594 1.406z"></path>';
+            arrowElement.innerHTML = '<path d="M9.875 0l16 16-16 16-3.75-3.75 12.25-12.25-12.25-12.25z"></path>';
         }
     }
 
