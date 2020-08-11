82ef8e6d658bc9dbe83113ad7013774bcbc7585e
Liang Ding <d@b3log.org>
2020-08-11 23:40:16 +0800

    :sparkles: 引入内容块（Block）和内容块引用（Block Reference） #87

--- app/src/graph/index.ts
+++ app/src/graph/index.ts
@@ -15,7 +15,38 @@ export class Graph {
 
     onGraph(liandi: ILiandi, data: any) {
         addScript(path.posix.join(Constants.APP_DIR, `vditore/dist/js/echarts/echarts.min.js`), "vditorEchartsScript").then(() => {
-            echarts.init(liandi.graph.element).setOption(data);
+            echarts.init(liandi.graph.element).setOption({
+                    title: {
+                        text: 'Graph 简单示例'
+                    },
+                    tooltip: {},
+                    animationDurationUpdate: 1500,
+                    animationEasingUpdate: 'quinticInOut',
+                    series: [
+                        {
+                            type: 'graph',
+                            layout: 'none',
+                            symbolSize: 50,
+                            roam: true,
+                            label: {
+                                show: true
+                            },
+                            edgeSymbol: ['circle', 'arrow'],
+                            edgeSymbolSize: [4, 10],
+                            edgeLabel: {
+                                fontSize: 20
+                            },
+                            data: data.data,
+                            links: data.links,
+                            lineStyle: {
+                                opacity: 0.9,
+                                width: 2,
+                                curveness: 0
+                            }
+                        }
+                    ]
+                }
+            );
         });
     }
 }
