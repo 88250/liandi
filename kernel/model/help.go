34f1853f4f87b606a70c3c3027c634be01299b85
Liang Ding <d@b3log.org>
2020-08-13 23:08:35 +0800

    :sparkles: 引入内容块（Block）和内容块引用（Block Reference） Fix #87

--- kernel/model/help.go
+++ kernel/model/help.go
@@ -15,7 +15,7 @@ import "path/filepath"
 func MountHelp() (url string) {
 	lang := Conf.Lang
 	lang = "zh_CN" // TODO: 目前只有中文帮助文档
-	url, _ = Mount("http://"+WebDAVLocalhostAddr+"/webdav/", filepath.Join(WorkingDir, "public/help/"+lang))
+	url, _ = Mount("http://"+WebDAVLocalhostAddr+"/webdav/", filepath.Join(WorkingDir, "app/public/"+lang+"/help"))
 	RestartServeWebDAV()
 	return
 }
