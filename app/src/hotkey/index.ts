c1b4c05a59f81009aa962fbe1fc50ee7bb84ea0a
Liyuan Li <v@b3log.org>
2020-08-14 22:35:48 +0800

    :lipstick:

--- app/src/hotkey/index.ts
+++ app/src/hotkey/index.ts
@@ -3,22 +3,19 @@ import {initSearch} from '../search';
 
 export const initGlobalKeyPress = (liandi: ILiandi) => {
     let lastKeypressTime = 0;
-    let timeoutId = 0;
     window.addEventListener('keydown', (event) => {
         // 快捷搜素
-        if (event.key === 'Shift') {
+        console.log(event)
+        if (event.key === 'Shift' && event.isComposing === false) {
             const thisKeypressTime = new Date().getTime();
             if (thisKeypressTime - lastKeypressTime <= Constants.DOUBLE_DELTA
                 && thisKeypressTime - lastKeypressTime >= 50) { // 防止 win32 长按
                 lastKeypressTime = 0;
-                timeoutId = window.setTimeout(() => {
-                    initSearch(liandi);
-                }, 200);
+                initSearch(liandi);
             }
             lastKeypressTime = thisKeypressTime;
         } else {
             lastKeypressTime = 0;
-            clearTimeout(timeoutId);
         }
     });
 };
