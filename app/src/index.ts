48caa7f127ead210e9fca082fa0411c64c42d3d7
Liyuan Li <v@b3log.org>
2020-08-14 10:05:43 +0800

    :art:

--- app/src/index.ts
+++ app/src/index.ts
@@ -142,7 +142,7 @@ class App {
             liandi.navigation.show();
             liandi.ws.send('mount', {
                 url: `${Constants.WEBDAV_ADDRESS}/`,
-                path: path.posix.join(Constants.APP_DIR, 'public/zh_CN/help')
+                path: path.posix.join(Constants.APP_DIR, 'public/zh_CN/帮助文档')
             });
         });
         document.getElementById('barBug').addEventListener('click', function () {
