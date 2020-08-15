5a60c80c3d1a6a6a1a234aec619ce52cda2ea89a
Liyuan Li <v@b3log.org>
2020-08-15 11:06:40 +0800

    :lipstick:

--- app/src/backlinks/index.ts
+++ app/src/backlinks/index.ts
@@ -1,5 +1,6 @@
 import * as path from 'path';
 import {i18n} from "../i18n";
+import {escapeHtml} from "../util/escape";
 
 export class Backlinks {
     private element = document.getElementById('backlinks') as HTMLDivElement;
@@ -28,7 +29,7 @@ export class Backlinks {
         } else {
             this.element.innerHTML = `<div class="backlinks__title">
 <div class="ft__secondary ft__smaller">${i18n[liandi.config.lang].backlinks}</div>
-<span>${liandi.current.path ? path.posix.join(path.posix.basename(liandi.current.dir.url), liandi.current.path) : ""}</span>
+<div class="fn__ellipsis">${escapeHtml(liandi.current.path ? path.posix.join(path.posix.basename(liandi.current.dir.url), liandi.current.path) : "")}</div>
 </div>
 <div class="backlinks__title"><div class="ft__secondary ft__smaller">${i18n[liandi.config.lang].noBacklinks}</div></div>`;
         }
@@ -37,18 +38,18 @@ export class Backlinks {
     public onBacklinks(liandi: ILiandi, backlinks: IBacklinks[]) {
         let backlinksHTML = `<div class="backlinks__title">
 <div class="ft__secondary ft__smaller">${i18n[liandi.config.lang].backlinks}</div>
-<span>${path.posix.join(path.posix.basename(liandi.current.dir.url), liandi.current.path)}</span>
+<div class="fn__ellipsis">${escapeHtml(path.posix.join(path.posix.basename(liandi.current.dir.url), liandi.current.path))}</div>
 </div>`
         backlinks.forEach((files) => {
             backlinksHTML += '<div class="item">'
             files.blocks.forEach((item, index) => {
                 if (index === 0) {
                     backlinksHTML += `<h2 data-type="backlinks-file" data-path="${encodeURIComponent(item.path)}" data-url="${encodeURIComponent(item.url)}" class="fn__flex"">
-<span class="fn__flex-1">${path.posix.basename(files.path)}</span>
-<span class="ft__smaller fn__flex-center">${path.posix.dirname(item.path).substr(1)}</span>
+<span class="fn__flex-1">${escapeHtml(path.posix.basename(files.path))}</span>
+<span class="ft__smaller fn__flex-center">${escapeHtml(path.posix.dirname(item.path).substr(1))}</span>
 </h2>`
                 }
-                backlinksHTML += `<div class="item__content fn__two-line">${item.content}</div>`
+                backlinksHTML += `<div class="item__content fn__two-line">${escapeHtml(item.content)}</div>`
             })
             backlinksHTML += '</div>'
         })
