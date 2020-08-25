1e74fd52791d35e98132cd2eb3430d2c6b66e54e
Liyuan Li <v@b3log.org>
2020-08-25 21:58:14 +0800

    :rotating_light:

--- app/electron/main.js
+++ app/electron/main.js
@@ -142,6 +142,8 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
+  } else {
+    globalShortcut.unregisterAll()
   }
 })
 
